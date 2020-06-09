5009d4e84216cadc53b15f65ab304644bcf31218
bccheung <bccheung@outlook.com>
2020-06-09 11:53:56 -0400

    Updated setup files and added license

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@ from setuptools import find_packages, setup
 
 setup(
     name='wsp-matrix-butler',
-    version='1.1',
+    version='1.1.0',
+    description='A SQLite-based mini-file system for organizing binary files for the Greater Golden Horseshoe Model',
+    url='https://github.com/wsp-sag/wsp-matrix-butler',
+    author='WSP',
+    maintatiner='Brian Cheung',
+    maintainer_email='brian.cheung@wsp.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ],
     packages=find_packages(),
-    python_requires='>=2.7',
     install_requires=[
         'pandas>=0.16',
         'numpy>=1.14'
-    ]
+    ],
+    python_requires='>=2.7'
 )
